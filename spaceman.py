dac6f5a48eb6e42ca45f358789b25e432a1a374d
Alex Rocha <rocha.amc@gmail.com>
2022-10-03 16:47:34 -0700

    Refactor based on feedback from pylint. Add character validation to guess input

--- spaceman.py
+++ spaceman.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 
 def load_word():
@@ -51,12 +52,12 @@ def get_guessed_word(secret_word, letters_guessed):
     """
 
     # Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
-    guessed_word = ''
+    guessed_word = ""
     for letter in secret_word:
         if letter in letters_guessed:
-            guessed_word += f'{letter} '
+            guessed_word += f"{letter} "
         else:
-            guessed_word += '_ '
+            guessed_word += "_ "
     return guessed_word
 
 
@@ -87,39 +88,44 @@ def spaceman(secret_word):
 
     """
 
-    # TODO: show the player information about the game according to the project spec
-    print('Welcome to Spaceman\n\nSpaceman is running out of air!\nHe needs to login to his spaceship computer to fix the air flow but he can\'t remember his password, and he doesn\'t use a password manager!\nQuick, help him remember his password before he runs out of guesses.')
-    letters_guessed = ''
+    # show the player information about the game according to the project spec
+    print(
+        "Welcome to Spaceman\n\nSpaceman is running out of air!\nHe needs to login to his spaceship computer to fix the air flow but he can't remember his password, and he doesn't use a password manager!\nQuick, help him remember his password before he runs out of guesses."
+    )
+    letters_guessed = ""
     incorrect_guesses_left = 7
-    print(f'\nGuess one letter at a time.\nYou have {incorrect_guesses_left} guesses to save Spaceman!')
+    print(
+        f"\nGuess one letter at a time.\nYou have {incorrect_guesses_left} guesses to save Spaceman!"
+    )
     game_over = False
-    while game_over == False:
+    while game_over is False:
         # Ask the player to guess one letter per round and check that it is only one letter
-        guess = 'False' # :)
-        while len(guess) != 1:
-            guess = input('\nGuess one letter: > ')
+        guess = "False"  # :)
+        while len(guess) != 1 or not re.match("^[a-zA-Z]", guess):
+            guess = input("\nGuess one letter: > ").lower()
         # Check if the guessed letter is in the secret or not and give the player feedback
-        if is_guess_in_word(guess, secret_word) == False:
+        if is_guess_in_word(guess, secret_word) is False:
             print("Failed! Spaceman was counting on you!")
             incorrect_guesses_left -= 1
-            print(f'Guesses left: {incorrect_guesses_left}')
+            print(f"Guesses left: {incorrect_guesses_left}")
             if incorrect_guesses_left == 0:
-                print(f'Spaceman! No! Spaceman! Spaceman? Spaceman...')
+                print("Spaceman! No! Spaceman! Spaceman? Spaceman...")
+                print(
+                    f"\nYou notice something under the keyboard...\nA sticky note with a password written on it: {secret_word}"
+                )
                 return False
         else:
-            print('Success! There is still hope')
+            print("Success! There is still hope")
             letters_guessed += guess
         # show the guessed word so far
         print(get_guessed_word(secret_word, letters_guessed))
         # check if the game has been won or lost
-        if is_word_guessed(secret_word, letters_guessed) == True:
-            print(f'Congratulations! You saved Spaceman!')
+        if is_word_guessed(secret_word, letters_guessed) is True:
+            print("Congratulations! The password worked! You saved Spaceman!")
             game_over = True
 
 
 # These function calls that will start the game
 secret_word = load_word()
 spaceman(secret_word)
-print(f'(The password was written on a sticky note under the keyboard: {secret_word})')
-print('\nGame Over')
-
+print("\nGame Over")
